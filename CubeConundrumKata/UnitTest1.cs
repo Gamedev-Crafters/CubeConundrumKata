759c2ba7fe3837d67217410d36dcda6b58f584ff
Javierdds <deduenassantanojavier@gmail.com>
2024-02-09 18:04:42 +0100

    feat: is game possible

--- CubeConundrumKata/UnitTest1.cs
+++ CubeConundrumKata/UnitTest1.cs
@@ -1,34 +1,35 @@
 namespace CubeConundrumKata;
 
 /*
- * 1. Número N1 de juegos, cada juego tiene un número N2 de puñados.
- * 2. En cada puñado se sacan un número x,y,z de cubos de una bolsa,
- *  siendo x = rojo, y = verde y z = azul.
- * 3. La bolsa cuenta con un número limitado de cubos de cada color.
- * 4. Hay que saber qué juegos son posibles.
+ * REGLAS DEL JUEGO:
+ * Hay un número N1 de juegos, cada juego tiene un número N2 de puñados.
+ * En cada puñado se sacan un número x,y,z de cubos de una bolsa,
+ * siendo x = rojo, y = verde y z = azul.
+ * La bolsa cuenta con un número limitado de cubos de cada color.
+ * 
+ * 1. Hay que saber qué juegos son posibles.
  *  Un juego es posible cuando puede contener el puñado del gnomo.
- * 5. Por último, sumar los IDs de los juegos posibles.
+ * 2. Sumar los IDs de los juegos posibles.
  */
 
 public class Tests
 {
     [Test]
-    public void Grab_Cubes()
+    public void gagsd()
     {
-        var sut = new Bag();
+        var sut = new Game();
 
-        var hand = sut.GrabCubes();
+        var isGamePossible = sut.IsGamePossible(12,13,14);
         
-        Assert.IsNotEmpty(hand);
+        Assert.IsTrue(isGamePossible);
     }
 
 }
 
-public class Bag
+public class Game
 {
-    
-    public int[] GrabCubes()
+    public bool? IsGamePossible(int i, int i1, int i2)
     {
-        return new int[3];
+        return true;
     }
 }
