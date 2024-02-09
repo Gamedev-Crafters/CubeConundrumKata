4876ff841c9f025b60bbde3c823f08c4a5533ec9
Javierdds <deduenassantanojavier@gmail.com>
2024-02-09 17:36:49 +0100

    feat: grab cubes from bag

--- CubeConundrumKata/UnitTest1.cs
+++ CubeConundrumKata/UnitTest1.cs
@@ -1,10 +1,40 @@
 namespace CubeConundrumKata;
 
+/*
+ * 1. Número N1 de juegos, cada juego tiene un número N2 de puñados.
+ * 2. En cada puñado se sacan un número x,y,z de cubos de una bolsa,
+ *  siendo x = rojo, y = verde y z = azul.
+ * 3. La bolsa cuenta con un número limitado de cubos de cada color.
+ * 4. Hay que saber qué juegos son posibles.
+ *  Un juego es posible cuando puede contener el puñado del gnomo.
+ * 5. Por último, sumar los IDs de los juegos posibles.
+ */
+
 public class Tests
 {
     [Test]
-    public void Test1()
+    public void Asadhnasd()
     {
-        Assert.Pass();
+        var sut = new Bag();
+
+        var hand = sut.GrabCubes();
+        
+        Assert.IsNotEmpty(hand);
+    }
+}
+
+public class Bag
+{
+    private List<string> _cubes;
+
+    public Bag()
+    {
+        _cubes = new List<string>();
+        _cubes.Add("");
+    }
+    
+    public List<string> GrabCubes()
+    {
+        return _cubes;
     }
 }
